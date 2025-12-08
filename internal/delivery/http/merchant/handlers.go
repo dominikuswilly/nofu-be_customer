ee20961ad8bf3330df4a56f8a5d9cdb7d71ba974
willy <medicalawn@gmail.com>
2025-12-08 21:09:11 +0700

    [refactor] http-merchant

--- internal/delivery/http/merchant/handlers.go
+++ internal/delivery/http/merchant/handlers.go
@@ -21,19 +21,6 @@ type MerchantHandler struct {
 	usecase *usecase.MerchantUsecase
 }
 
-// NewMerchantHandler registers routes under /api
-func NewMerchantHandler(r *mux.Router, u *usecase.MerchantUsecase) {
-	handler := &MerchantHandler{usecase: u}
-	api := r.PathPrefix("/api").Subrouter()
-
-	api.HandleFunc("/merchants", handler.Create).Methods(http.MethodPost)
-	api.HandleFunc("/merchants", handler.GetAll).Methods(http.MethodGet)
-	api.HandleFunc("/merchants/{id}", handler.GetByID).Methods(http.MethodGet)
-	api.HandleFunc("/merchants/{id}", handler.Update).Methods(http.MethodPut)
-	api.HandleFunc("/merchants/{id}", handler.Delete).Methods(http.MethodDelete)
-	api.HandleFunc("/merchants/login", handler.Login).Methods(http.MethodPost)
-}
-
 func (h *MerchantHandler) Create(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
---
+++ internal/delivery/http/merchant/routes.go
@@ -0,0 +1,21 @@
+package merchant
+
+import (
+	"be_customer/internal/usecase"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// NewMerchantHandler registers merchant routes under /api
+func NewMerchantHandler(r *mux.Router, u *usecase.MerchantUsecase) {
+	handler := &MerchantHandler{usecase: u}
+	api := r.PathPrefix("/api").Subrouter()
+
+	api.HandleFunc("/merchants", handler.Create).Methods(http.MethodPost)
+	api.HandleFunc("/merchants", handler.GetAll).Methods(http.MethodGet)
+	api.HandleFunc("/merchants/{id}", handler.GetByID).Methods(http.MethodGet)
+	api.HandleFunc("/merchants/{id}", handler.Update).Methods(http.MethodPut)
+	api.HandleFunc("/merchants/{id}", handler.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("/merchants/login", handler.Login).Methods(http.MethodPost)
+}
